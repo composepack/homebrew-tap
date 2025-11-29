c01c87f4a28d2f7fb2956c7e4001eb210867bab8
Xiyuan Chen <52963600+GareArc@users.noreply.github.com>
2025-11-29 00:57:42 +0000

    composepack v0.0.4: update formula

--- Formula/composepack.rb
+++ Formula/composepack.rb
@@ -1,15 +1,26 @@
 class Composepack < Formula
   desc "Helm-style templating and packaging for Docker Compose"
   homepage "https://github.com/composepack/composepack"
-  version "0.0.3"
+  version "0.0.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/composepack/composepack/releases/download/v0.0.3/composepack_v0.0.3_darwin_arm64.tar.gz"
-      sha256 "648833e3a47afd7e52d2df77d63c3e43d5f37671f15e3901d8525de8b825387d"
+      url "https://github.com/composepack/composepack/releases/download/v0.0.4/composepack_v0.0.4_darwin_arm64.tar.gz"
+      sha256 "55cc9466a3c19176027de5ce8a6446020a7550a090b633103594500e4e9d26df"
     else
-      url "https://github.com/composepack/composepack/releases/download/v0.0.3/composepack_v0.0.3_darwin_amd64.tar.gz"
-      sha256 "f5d0e4c15a55e348b47b940899fe8556c1e9f0c1d76efb17153da5d55666f742"
+      url "https://github.com/composepack/composepack/releases/download/v0.0.4/composepack_v0.0.4_darwin_amd64.tar.gz"
+      sha256 "ad79e7205206e545f604593cef41ba04641934fc60245207f9ffa712fa09c2ca"
+    end
+  end
+
+  # Linux support when shas are provided
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/composepack/composepack/releases/download/v0.0.4/composepack_v0.0.4_linux_arm64.tar.gz"
+      sha256 "fd1165b74ee8921f8a6371c3888b3045422cb41faf90112bf739c81940986e98"
+    else
+      url "https://github.com/composepack/composepack/releases/download/v0.0.4/composepack_v0.0.4_linux_amd64.tar.gz"
+      sha256 "449a5e26cf6f8af0ef68c3b22e94acadc4a4569be368b9392ab5153aa24e6c98"
     end
   end
 
