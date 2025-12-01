103ec10496f0be0a32a9d82672e19e981e57e803
Xiyuan Chen <52963600+GareArc@users.noreply.github.com>
2025-12-01 08:20:51 +0000

    composepack v0.0.5: update formula

--- Formula/composepack.rb
+++ Formula/composepack.rb
@@ -1,26 +1,26 @@
 class Composepack < Formula
   desc "Helm-style templating and packaging for Docker Compose"
   homepage "https://github.com/composepack/composepack"
-  version "0.0.4"
+  version "0.0.5"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/composepack/composepack/releases/download/v0.0.4/composepack_v0.0.4_darwin_arm64.tar.gz"
-      sha256 "55cc9466a3c19176027de5ce8a6446020a7550a090b633103594500e4e9d26df"
+      url "https://github.com/composepack/composepack/releases/download/v0.0.5/composepack_v0.0.5_darwin_arm64.tar.gz"
+      sha256 "6390c433fc84edb9a09242303e2c73c379c765dc9279d55bf5f3228689dce1a4"
     else
-      url "https://github.com/composepack/composepack/releases/download/v0.0.4/composepack_v0.0.4_darwin_amd64.tar.gz"
-      sha256 "ad79e7205206e545f604593cef41ba04641934fc60245207f9ffa712fa09c2ca"
+      url "https://github.com/composepack/composepack/releases/download/v0.0.5/composepack_v0.0.5_darwin_amd64.tar.gz"
+      sha256 "f9f112665d3103f7d85325e75cec289c83ac841308d760fb50c662da2cf3a59e"
     end
   end
 
   # Linux support when shas are provided
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/composepack/composepack/releases/download/v0.0.4/composepack_v0.0.4_linux_arm64.tar.gz"
-      sha256 "fd1165b74ee8921f8a6371c3888b3045422cb41faf90112bf739c81940986e98"
+      url "https://github.com/composepack/composepack/releases/download/v0.0.5/composepack_v0.0.5_linux_arm64.tar.gz"
+      sha256 "9088d281daaf32246a1a7c83c16198fd2126296299ad852b86e957541d89e0c7"
     else
-      url "https://github.com/composepack/composepack/releases/download/v0.0.4/composepack_v0.0.4_linux_amd64.tar.gz"
-      sha256 "449a5e26cf6f8af0ef68c3b22e94acadc4a4569be368b9392ab5153aa24e6c98"
+      url "https://github.com/composepack/composepack/releases/download/v0.0.5/composepack_v0.0.5_linux_amd64.tar.gz"
+      sha256 "102ecd7d0739e74d51768c6d0b70ff8cb3cff05cacf6e586c87d99f5ddcc4db5"
     end
   end
 
